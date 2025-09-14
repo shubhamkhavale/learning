6f7312bd75807f251fe4b7bc6701a5851715a59b
Shubham Khavale <khavales12@gmail.com>
2025-09-14 02:32:31 +0000

    01_variable

---
+++ 01_variable/test.js
@@ -0,0 +1,7 @@
+const accountID = 1234;
+let userName="shubham khavale";
+const userPass="Shubham@123";
+var userEmail="shubhamkx12@gmail.com";
+let userstate;
+console.log(userName);
+console.table([accountID,userEmail,userName,userPass,userstate]);
