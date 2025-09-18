b03d4318d0680329274971f92c917cb19164bf3e
Shubham Khavale <khavales12@gmail.com>
2025-09-18 00:52:05 +0000

    Datatype Conversion

--- 01_variable/02_datatypes.js
+++ 01_variable/02_datatypes.js
@@ -1,3 +1,15 @@
 //console.log("shubham Rakesh khavale");
  let a = "shubham khavale";
 console.log( a,typeof 'a');
+
+let num = 100;
+const valueofNum = Number(num);
+console.log( typeof valueofNum);
+
+let Snum  = "shubham";
+const stringSnum = String(Snum);
+console.log( typeof Snum);
+
+let bNum = "shubham";
+let booleanIsbNum = Boolean(bNum);
+console.log(booleanIsbNum);
